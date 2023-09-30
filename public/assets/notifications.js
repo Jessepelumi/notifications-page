8a99af6e753e2378af691de6b0387ec871dc21c8
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2023-10-01 00:08:17 +0100

    created pseudo db

--- public/assets/notifications.js
+++ public/assets/notifications.js
@@ -2,6 +2,50 @@ const notificationsDb = [
     {
         "name": "Mark Webber",
         "message": "reacted to your recent post",
-        "post": "My first tournament today!"
+        "post": "My first tournament today!",
+        "time": "1m ago",
+        "content": "lorem ipsum dolor sit amet, consectetur. Lorem ipsum dolor sit. Lorem ipsum dolor sit. Lorem ipsum d. Lorem ipsum d. Lorem ipsum d. Lorem"
+    },
+    {
+        "name": "Mark Webber",
+        "message": "reacted to your recent post",
+        "post": "My first tournament today!",
+        "time": "1m ago",
+        "content": "lorem ipsum dolor sit amet, consectetur. Lorem ipsum dolor sit. Lorem ipsum dolor sit. Lorem ipsum d. Lorem ipsum d. Lorem ipsum d. Lorem"
+    },
+    {
+        "name": "Mark Webber",
+        "message": "reacted to your recent post",
+        "post": "My first tournament today!",
+        "time": "1m ago",
+        "content": "lorem ipsum dolor sit amet, consectetur. Lorem ipsum dolor sit. Lorem ipsum dolor sit. Lorem ipsum d. Lorem ipsum d. Lorem ipsum d. Lorem"
+    },
+    {
+        "name": "Mark Webber",
+        "message": "reacted to your recent post",
+        "post": "My first tournament today!",
+        "time": "1m ago",
+        "content": "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game."
+    },
+    {
+        "name": "Mark Webber",
+        "message": "reacted to your recent post",
+        "post": "My first tournament today!",
+        "time": "1m ago",
+        "content": "lorem ipsum dolor sit amet, consectetur. Lorem ipsum dolor sit. Lorem ipsum dolor sit. Lorem ipsum d. Lorem ipsum d. Lorem ipsum d. Lorem"
+    },
+    {
+        "name": "Mark Webber",
+        "message": "reacted to your recent post",
+        "post": "My first tournament today!",
+        "time": "1m ago",
+        "content": "lorem ipsum dolor sit amet, consectetur. Lorem ipsum dolor sit. Lorem ipsum dolor sit. Lorem ipsum d. Lorem ipsum d. Lorem ipsum d. Lorem"
+    },
+    {
+        "name": "Mark Webber",
+        "message": "reacted to your recent post",
+        "post": "My first tournament today!",
+        "time": "1m ago",
+        "content": "lorem ipsum dolor sit amet, consectetur. Lorem ipsum dolor sit. Lorem ipsum dolor sit. Lorem ipsum d. Lorem ipsum d. Lorem ipsum d. Lorem"
     }
 ];
