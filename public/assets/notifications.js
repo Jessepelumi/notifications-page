b9d89e1ff0e2365e2f6c56d5571f8959879acd97
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2023-10-03 14:36:41 +0100

    added json file

--- public/assets/notifications.js
+++ public/assets/notifications.js
@@ -1,94 +1,43 @@
 
-// notification panel
-const notifications = [
-    {
-        picture: "",
-        name: "Mark Webber",
-        message: "reacted to your recent post",
-        post: "My first tournament today!",
-        time: "1m ago",
-        content: " "
-    },
-    {
-        picture: "",
-        name: "Angela Gray",
-        message: "followed you",
-        post: " ",
-        time: "5m ago",
-        content: " "
-    },
-    {
-        picture: "",
-        name: "Jacob Thompson",
-        message: "has joined your group",
-        post: "Chess Club",
-        time: "1 day ago",
-        content: " "
-    },
-    {
-        picture: "",
-        name: "Rizky Hasanuddin",
-        message: "sent you a private message",
-        post: "My first tournament today!",
-        time: "5 days ago",
-        content: "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game."
-    },
-    {
-        picture: "",
-        name: "Kimberly Smith",
-        message: "commented on your picture",
-        post: "My first tournament today!",
-        time: "1 week ago",
-        content: " "
-    },
-    {
-        picture: "",
-        name: "Nathan Peterson",
-        message: "reacted to your recent post",
-        post: "5 end-game strategies to increase your win rate",
-        time: "2 weeks ago",
-        content: " "
-    },
-    {
-        picture: "",
-        name: "Anna Kim",
-        message: "left the group",
-        post: "Chess Club",
-        time: "2 weeks ago",
-        content: " "
-    }
-];
+fetch("assets/notifications.json")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Couldn't fetch data");
+        }
+        return response.json();
+    })
+    .then(data => {
 
-const container = document.getElementById("container");
+        const notifications = data.map(notification => {
+            const notificationBox = document.createElement("div");
+            notificationBox.classList.add("notification-box");
 
-for (let notification of notifications) {
-    const notificationBox = document.createElement("div");
-    notificationBox.classList.add("notification-box");
+            const pictureEl = document.createElement("div");
+            const picture = document.createElement("img");
+            picture.src = notification.picture;
+            picture.alt = notification.name;
+            pictureEl.classList.add("picture-element");
+            picture.classList.add("picture");
+            pictureEl.append(picture);
 
-    const pictureEl = document.createElement("div");
-    const picture = document.createElement("img");
-    picture.src = notification.picture;
-    picture.alt = notification.name;
-    pictureEl.classList.add("picture-element");
-    picture.classList.add("picture");
-    pictureEl.appendChild(picture);
+            const notifyEl = document.createElement("div");
+            const notify = document.createElement("div");
+            const time = document.createElement("div");
+            const name = document.createElement("span");
+            const message = document.createElement("span");
+            const post = document.createElement("span");
+            name.innerText = notification.name;
+            message.innerText = notification.message;
+            post.innerText = notification.post;
+            time.innerText = notification.time;
+            notifyEl.classList.add("notify-element");
+            notify.append(name, message, post);
+            notifyEl.append(notify, time);
 
-    const notifyEl = document.createElement("div");
-    const notify = document.createElement("div");
-    const time = document.createElement("div");
-    const name = document.createElement("span");
-    const message = document.createElement("span");
-    const post = document.createElement("span");
-    name.innerText = notification.name;
-    message.innerText = notification.message;
-    post.innerText = notification.post;
-    time.innerText = notification.time;
-    notifyEl.classList.add("notify-element");
-    notify.appendChild(name, message, post);
-    notifyEl.appendChild(notify, time);
+            notificationBox.append(pictureEl, notifyEl);
 
-    notificationBox.appendChild(pictureEl, notifyEl);
+            container.append(notificationBox);
+        })
 
-    container.appendChild(notificationBox);
-
-};
+    })
+    .catch(error => console.log("Error loading JSON: ", error.message));
