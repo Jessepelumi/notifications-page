8070145ac9da03ad3576f0035760eac4bb2a14c2
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2023-10-07 19:35:48 +0100

    nearly done with new js file

--- public/assets/js/notifications.js
+++ public/assets/js/notifications.js
@@ -1,4 +1,7 @@
 
+const container = document.getElementById("container");
+const notificationCount = document.getElementById("notification-count");
+const markAsRead = document.getElementById("mark-as-read");
 
 const renderPosts = async () => {
     let uri = "http://localhost:3000/notifications";
@@ -6,6 +9,62 @@ const renderPosts = async () => {
     const response = await fetch(uri);
     const notifications = await response.json();
     console.log(notifications);
+
+    let template = "";
+    notifications.forEach(notification => {
+        template += `
+            <div class="notification-box">
+            
+                <div class="picture-element">
+                    <img src="${notification.picture}" alt="${notification.name}" class="picture" >
+                </div>
+
+                <div class="notify-element">
+                    <div class="notify">
+                        <div>
+                            <div>
+                                <span class="name">
+                                    ${notification.name}
+                                </span>
+                                <span class="message">
+                                    ${notification.message}
+                                </span>
+                                <span class="post">
+                                    ${notification.post}
+                                </span>
+                                <span class="group">
+                                    ${notification.group}
+                                </span>
+                            </div>
+                        </div>
+                        <span class="image">
+                            ${notification.image}
+                        </span>
+                        
+                    </div>
+                    <div class="time">
+                        ${notification.time}
+                    </div>
+                    <div class="content">
+                        ${notification.content}
+                    </div>
+                </div>
+            
+            </div>
+
+        `
+
+
+    });
+
+    container.innerHTML = template;
+
+    const contentDiv = container.querySelector(".content");
+    console.log(contentDiv.innerHTML);
+    if(contentDiv.innerText === "") {
+        contentDiv.classList.remove("content");
+    };
+
 };
 
 window.addEventListener("DOMContentLoaded", () => renderPosts());
